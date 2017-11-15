8b5a11c855f662bd2fc39503ae89fc3da07866d1
Mohd Fikrihafifi <fikri@kkr.gov.my>
2017-11-15 10:30:34 +0800

    First commit
    For the first time commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, LoadingController } from 'ionic-angular';
+import { WebserviceProvider } from '../../providers/webservice/webservice';
+import { DetailsPage } from '../details/details';
+import { AddPage } from '../add/add';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  staffList: Array<any>;
 
+  constructor(public navCtrl: NavController, public webservice: WebserviceProvider, public modalCtrl: ModalController, public loadingCtrl: LoadingController) {
+    this.staffList = []
   }
 
+  ionViewDidLoad(){
+    let loader = this.loadingCtrl.create({
+      //content: "Sila Tunggu...",
+      spinner: 'dots'
+      //duration: 3000
+    });
+    loader.present();
+
+    this.webservice.getEmployee().then(response => {
+      console.log(response);
+      this.staffList = response.person;
+      loader.dismiss();
+    }).catch(error => {
+      alert('Error' + error);
+    });
+  }
+
+  detailsPage(item: any){
+    this.navCtrl.push(DetailsPage, {item: item});
+  }
+
+  pageAdd(){
+    let modal = this.modalCtrl.create(AddPage);
+    modal.present();
+
+    modal.onDidDismiss( employee => {
+      if(employee){
+        this.webservice.saveEmployee(employee).then(response => {
+          console.log('Message: ' + JSON.stringify(response)); //response dlm bentuk JSON --> stringify convert to string - senang debug
+        })
+      } 
+    });
+  }
 }
+/*
+    .catch(error => {
+      alert('Error: ' + error);//error handling
+*/
